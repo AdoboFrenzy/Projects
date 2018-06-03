4658ed5a367903274b927c325ee31748e9ba2ee1
Alfred Alejandrino <alejandrino.alfred@gmail.com>
2018-06-03 10:29:02 -0700

    remove files

--- frontend/middleware/thunk.js
+++
@@ -1,7 +0,0 @@
-export const thunk = ({dispatch}) => next => action => {
-  if (typeof action === 'function') {
-    return action(dispatch);
-  }
-
-  return next(action);
-};
--- frontend/reducers/selectors.js
+++
@@ -1,6 +0,0 @@
-// import values from 'lodash/values';
-
-
-export const selectAllPokemon = (state) => {
-  return Object.values(state.entities.pokemon);
-};
