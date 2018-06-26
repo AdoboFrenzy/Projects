6ef717554c1617896cd10ee6fe20946ac3f9fb71
AdoboFrenzy <alejandrino.alfred@gmail.com>
2018-06-25 21:28:46 -0700

    connect mongodb w/ mongoose

---
+++ devconnector/config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI:
+    "mongodb://Alfred:alfred123@ds018558.mlab.com:18558/parkingsniffers"
+};
--- devconnector/server.js
+++ devconnector/server.js
@@ -1,8 +1,18 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello'));
+// DB Config
+const db = require('./config/keys.js').mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log('MongoDB Connected'))
+  .catch(err => console.log(err))
+
+app.get('/', (req, res) => res.send('Hello world!'));
 
 const port = process.env.port || 5000;
 
