057f860e4fa9a967a55be0f11b4a665d87f7ff8b
AdoboFrenzy <alejandrino.alfred@gmail.com>
2018-08-10 20:17:10 -0700

    add feature to encrypt user password

--- cleaningApp/config/keys.js
+++ cleaningApp/config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     mongoURI: 
-    'mongodb://datrupnoy:alfred123@ds018568.mlab.com:18568/cleaningapp'
+    'mongodb://datrupnoy:alfred123@ds018568.mlab.com:18568/cleaningapp',
+    secretOrKey: 'secret'
 };
