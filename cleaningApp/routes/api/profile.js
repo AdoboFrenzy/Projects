4e634b29bb4351ccaf1b2e233cccad328e4928c5
AdoboFrenzy <alejandrino.alfred@gmail.com>
2018-08-16 15:13:19 -0700

    finish

--- cleaningApp/routes/api/profile.js
+++ cleaningApp/routes/api/profile.js
@@ -11,11 +11,24 @@ const User = require('../../models/User');
 // @route   GET api/profile/test
 // @desc    Tests profile route
 // @access  Public
-
 router.get('/test', (req, res) => res.json({ msg: "Profile Works" }));
 
 // @route   GET api/profile
-// @desc    Tests profile route
-// @access  Public
+// @desc    Get current user's profile
+// @access  Private
+router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const errors = {};
+
+    Profile.findOne({ user: req.user.id })
+        .then(profile => {
+            if(!profile) {
+                errors.noprofile = 'There is no profile for this user';
+                return res.status(404).json(errors);
+            }
+            res.json(profile);
+        })
+        .catch(err => res.status(404).json(err));
+});
+
 
 module.exports = router;
