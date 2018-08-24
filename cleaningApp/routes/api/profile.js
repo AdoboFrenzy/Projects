017eaeff1a4f6c7d6c3847aac02d92998827ed20
AdoboFrenzy <alejandrino.alfred@gmail.com>
2018-08-23 18:34:04 -0700

    handle backend route

--- cleaningApp/routes/api/profile.js
+++ cleaningApp/routes/api/profile.js
@@ -39,7 +39,19 @@ router.get('/', passport.authenticate('jwt', { session: false }), (req, res) => 
 // @desc    Get profile by handle
 // @access  Public
 router.get('/handle/:handle', (req, res) => {
+    const errors = {};
+
     Profile.findOne({ handle: req.params.handle })
+        .populate('user', ['name', 'avatar'])
+        .then(profile => {
+            if(!profile) {
+                errors.noprofile = 'There is no profile for this user';
+                res.status(404).json(errors);
+            }
+
+            res.json(profile);
+        })
+        .catch(err => res.status(404).json(err))
 })
 
 
