57075b2f5c3f5d12c3c5b3cb4d3aaae7ba983373
AdoboFrenzy <alejandrino.alfred@gmail.com>
2018-09-07 12:13:16 -0700

    updates

--- cleaningApp/validation/is-empty.js
+++ cleaningApp/validation/is-empty.js
@@ -1,6 +1,5 @@
 const isEmpty = (value) => {
     return (
-        
         value === undefined ||
         value === null ||
         (typeof value === 'object' && Object.keys(value).length === 0) ||
