534a50d1606706287e0e7e72b9413e479c2f6eb1
AdoboFrenzy <alejandrino.alfred@gmail.com>
2018-09-28 13:57:32 -0700

    modify routes api posts

--- cleaningApp/routes/api/posts.js
+++ cleaningApp/routes/api/posts.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+const Post = require('../../models/Post');
 
 // @route   GET api/posts/test
 // @desc    Tests post route
 // @access  Public
 
 router.get('/test', (req, res) => res.json({ msg: "Posts Works" }));
+
+
+// @route   POST api/posts
+// @desc    Create post
+// @access  Private
+
+router.post('/', passport.authenticate('jwt', { session: false }), (req, res) => {
+    const newPost = 
+});
+
 
 module.exports = router;
